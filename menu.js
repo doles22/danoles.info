5afd14d675150a5bd24829ad6bc7a53059c079d1
doles22 <olesfamily614@gmail.com>
2019-10-31 15:26:36 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,33 +1,9 @@
-(function mobileMenu() {
-    
-    // Create mobile element
-    var mobile = document.createElement('div');
-    mobile.className = 'nav-mobile';
-    document.querySelector('.nav').appendChild(mobile);
+function mobileMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
-    // hasClass
-    function hasClass(elem, className) {
-        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-    }
-
-    // toggleClass
-    function toggleClass(elem, className) {
-        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-        if (hasClass(elem, className)) {
-            while (newClass.indexOf(' ' + className + ' ') >= 0) {
-                newClass = newClass.replace(' ' + className + ' ', ' ');
-            }
-            elem.className = newClass.replace(/^\s+|\s+$/g, '');
-        } else {
-            elem.className += ' ' + className;
-        }
-    }
-
-    // Mobile nav function
-    var mobileNav = document.querySelector('.nav-mobile');
-    var toggle = document.querySelector('.nav-list');
-    mobileNav.onclick = function () {
-        toggleClass(this, 'nav-mobile-open');
-        toggleClass(toggle, 'nav-active');
-    };
-})();
