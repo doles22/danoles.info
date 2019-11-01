dc8a86a8a7ccf74852f795b93e06b5cbdaa1ec7c
doles22 <olesfamily614@gmail.com>
2019-11-01 15:45:06 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,13 +2,9 @@ function mobileMenu() {
   var x = document.getElementById("h_bar");
   if (x == null) {
     x = document.getElementById("h_barshow");
+    x.id = "h_bar";
   }
   else {
-    if (x.id === "h_bar") {
     x.id = "h_barshow";
-    }
-    else {
-      x.id = "h_bar";
-    }
   }
 }
