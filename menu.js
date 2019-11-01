3cd1e0c89b6bf98feba9c7e0f09c299d9aa01ea8
doles22 <olesfamily614@gmail.com>
2019-11-01 15:40:00 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,8 @@
 function mobileMenu() {
   var x = document.getElementById("h_bar");
-  if (x = null) {
+  if (x == null) {
     x = document.getElementById("h_barshow");
+  }
   else {
     if (x.id === "h_bar") {
     x.id = "h_barshow";
